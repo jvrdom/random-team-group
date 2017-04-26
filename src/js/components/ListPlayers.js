0551c33f528d733c7c860d0dac55817d5d35a7bc
Javier Dominguez <jvrdom@gmail.com>
2017-04-25 22:53:55 -0300

    Change the way to interact with components

---
+++ src/js/components/ListPlayers.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Player from "./Player";
+
+export default class ListPlayers extends React.Component {
+  render() {
+    const empty = this.props.players.length == 0;
+    const Players = this.props.players.map((name, i) => <Player key={i} name={name}/> );
+    const btnDisableClass = empty ? "disabled" : "";
+    return (
+      <div>
+        <h1>Players</h1>
+        <div class="row">{Players}</div>
+        <button type="button" class={"btn " + btnDisableClass} name="button" onClick={this.props.removePlayer}>Choose</button>
+      </div>
+    );
+  }
+}
--- src/js/pages/Index.js
+++ src/js/pages/Index.js
@@ -1,13 +1,30 @@
 import React from "react";
 
-import TablePlayers from "../components/TablePlayers";
+import ListPlayers from "../components/ListPlayers";
 import ListGroups from "../components/ListGroups";
 
 export default class Index extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      players : [
+        "Javier",
+        "Jorge",
+        "Stefania"
+      ]
+    }
+  }
+
+  removePlayer(e) {
+    const randomAd = this.state.players[Math.round( Math.random() * (this.state.players.length-1) )];
+    let arr = this.state.players.filter(item => item !== randomAd)
+    this.setState({players: arr})
+  }
+
   render() {
     return (
       <div>
-        <TablePlayers/>
+        <ListPlayers players={this.state.players} removePlayer={this.removePlayer.bind(this)}/>
         <ListGroups/>
       </div>
     );
