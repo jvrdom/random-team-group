115b0c146851083dc25ff9e091925f8c7b3caadc
Javier Dominguez <jvrdom@gmail.com>
2017-06-26 12:47:33 -0300

    Some eslint stuffs

--- src/js/reducers/groupsReducer.js
+++ src/js/reducers/groupsReducer.js
@@ -1,25 +1,25 @@
-export default function reducer(state={
-    groups: false,
-    fetching: false,
-    fetched: false,
-    error: null,
-  }, action) {
-    switch (action.type) {
-      case "FETCH_GROUPS": {
-        return {...state, fetching: true}
-      }
-      case "FETCH_GROUPS_REJECTED": {
-        return {...state, fetching: false, error: action.payload}
-      }
-      case "FETCH_GROUPS_FULFILLED": {
-        return {
-          ...state,
-          fetching: false,
-          fetched: true,
-          groups: action.payload,
-        }
-      }
+export default function reducer(state = {
+  groups: false,
+  fetching: false,
+  fetched: false,
+  error: null
+}, action) {
+  switch (action.type) {
+    case "FETCH_GROUPS": {
+      return {...state, fetching: true };
     }
+    case "FETCH_GROUPS_REJECTED": {
+      return { ...state, fetching: false, error: action.payload };
+    }
+    case "FETCH_GROUPS_FULFILLED": {
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        groups: action.payload
+      };
+    }
+  }
 
-    return state
+  return state;
 }
